99a4b486279c4e9bfea23119c33418438dcf10fd
Adrien <adrien.ysnel@gmail.com>
2017-01-18 10:12:49 +0100

    start bulk implementation + ignore .idea files

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -2,11 +2,15 @@ package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 import io.searchbox.client.JestClient;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Index;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CompletionLoader {
@@ -22,13 +26,26 @@ public class CompletionLoader {
         String inputFilePath = args[0];
         JestClient client = ElasticSearchRepository.createClient();
 
+        ArrayList<Index> list = new ArrayList<>();
+
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
             bufferedReader.lines()
                     .forEach(line -> {
                         //TODO ElasticSearch insert
-                        System.out.println(line);
+                        //System.out.println(line);
+                        list.add(new Index.Builder(line).build());
                     });
         }
+
+        Bulk bulk = new Bulk.Builder()
+                .defaultIndex("Actor")
+                .defaultType("name")
+                .addAction(list)
+                .build();
+
+        System.out.println("before execute");
+        client.execute(bulk);
+        System.out.println("after execute");
 
         System.out.println("Inserted total of " + count.get() + " actors");
     }
